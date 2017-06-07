bff199492be7194bd2b5117fd321387be5316826
Daniel Freiling <dfg@nota.dk>
2017-06-07 10:42:13 +0200

    fix(preloading): only try to preload once
    didn’t cause any bugs, just wasn’t necessary

--- AudioPlayer/AudioPlayer/player/extensions/AudioPlayer+Preload.swift
+++ AudioPlayer/AudioPlayer/player/extensions/AudioPlayer+Preload.swift
@@ -17,27 +17,29 @@ extension AudioPlayer {
         cachedAssets = [:]
     }
     
-    public func getAVURLAsset(forUrl: URL) -> AVURLAsset {
+    func getAVURLAsset(forUrl: URL) -> AVURLAsset {
         if let asset = cachedAssets[forUrl],
                asset.isPlayable {
             return asset
         } else {
+            ///See options: https://developer.apple.com/reference/avfoundation/avurlasset/initialization_options
             let asset = AVURLAsset(url: forUrl)
             cachedAssets[forUrl] = asset
             return asset
         }
     }
     
-    public func getAVPlayerItem(forUrl: URL) -> AVPlayerItem {
+    func getAVPlayerItem(forUrl: URL) -> AVPlayerItem {
         let asset = getAVURLAsset(forUrl: forUrl)
         let playerItem = AVPlayerItem(asset: asset, automaticallyLoadedAssetKeys: AudioPlayer.assetPreloadKeys)
         if #available(iOS 10.0, tvOS 10.0, OSX 10.12, *) {
             playerItem.preferredForwardBufferDuration = self.preferredForwardBufferDuration
         }
+        
         return playerItem
     }
     
-    public func preloadItemAsset(asset: AVURLAsset, onComplete: @escaping (AVURLAsset?) -> Void) {
+    func preloadItemAsset(asset: AVURLAsset, onComplete: @escaping (AVURLAsset?) -> Void) {
         asset.loadValuesAsynchronously(forKeys: AudioPlayer.assetPreloadKeys) {
             if (self.assetPreloadKeysAreLoaded(asset: asset) == false) {
                 //loading failed, invalidate cached asset
@@ -49,19 +51,24 @@ extension AudioPlayer {
         }
     }
     
-    public func preloadNextItemAsset() {
-        if hasNext, let queue = queue {
-            let nextPosition = queue.nextPosition,
-                item = queue.items[nextPosition],
-                urlInfo = item.highestQualityURL,
-                asset = getAVURLAsset(forUrl: urlInfo.url)
-            print("preloading queue idx: \(nextPosition)")
-            preloadItemAsset(asset: asset) { asset in
-                if (asset == nil) {
-                    print("error preloading queue idx: \(nextPosition)")
-                } else {
-                    print("preloaded queue idx: \(nextPosition)!")
-                }
+    func preloadNextItemAsset() {
+        guard hasNext, let queue = queue else {
+            return
+        }
+        let nextPosition = queue.nextPosition,
+            item = queue.items[nextPosition],
+            urlInfo = item.highestQualityURL
+        if cachedAssets[urlInfo.url] != nil {
+            // Next item has already been preloaded
+            return
+        }
+        print("preloading queue idx: \(nextPosition)")
+        let asset = getAVURLAsset(forUrl: urlInfo.url)
+        preloadItemAsset(asset: asset) { asset in
+            if (asset == nil) {
+                print("error preloading queue idx: \(nextPosition)")
+            } else {
+                print("preloaded queue idx: \(nextPosition)!")
             }
         }
     }
